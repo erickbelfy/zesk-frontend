040a3c71ade90cf1128a38fdf148cc5a76013ca7
Erick Belfort <erick.belf@gmail.com>
2018-06-15 15:58:56 -0400

    adding sass support, first implementation

--- src/components/RootComponent.js
+++ src/components/RootComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from 'react-bootstrap/lib/Grid';
 import ErrorComponent from './common/ErrorComponent';
 import RouteContainer from './RouteContainer';
+import './RootComponent.scss';
 
 export class RootComponent extends ErrorComponent {
     render() {
--- src/components/screens/credit-health/index.js
+++ src/components/screens/credit-health/index.js
@@ -6,7 +6,14 @@ class CreditHealth extends Component {
     render() {
         return (
             <section id="credit-health">
-            oi
+              <Row>
+                <Col lg={3} sm={4}>
+                  List of credit cards
+                </Col>
+                <Col lg={3} sm={4}>
+                  list of expenses
+                </Col>
+              </Row>
             </section>
         );
     }
